51ebaf2aa4afb718171541a35b4973b6eabd024e
Aj <adtune6509@student.waynecc.edu>
2018-05-02 17:15:02 -0400

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,60 @@
 //
+socket.on('not-logged-in', function() {
+    $('.login-form').css({
+        'display': 'inherit'
+    });
+})
+$("#loginForm").submit(function(e) {
+    e.preventDefault();
+});
+
+function login() {
+    var loginData = {
+        email: $("#email").val(),
+
+        password: $("#password").val()
+    }
+    socket.emit('returning-user', loginData)
+}
+socket.on('login-error', function(msg) {
+    toastr.error(msg);
+})
+socket.on('login-success', function(username) {
+    $('.login-form').css({
+        'display': 'none'
+    });
+    toastr.success('Successfully logged in as ' + username);
+})
+socket.on('not-logged-in-msg', function() {
+    toastr.error("You cannot chat if you're not signed in.")
+})
+
+function callSignUp() {
+    $('.login-form').css({
+        'display': 'none'
+    });
+    $('.signup-form').css({
+        'display': 'inherit'
+    });
+}
+
+function signup() {
+    var signUpData = {
+        email: $('#signupemail').val(),
+
+        password: $('#signuppassword').val(),
+
+        displayName: $('#displayname').val()
+
+    }
+    socket.emit('new-user', signUpData);
+}
+socket.on('signup-error', function(error) {
+    toastr.error(error);
+})
+socket.on('signup-success', function() {
+    toastr.success('Sign up successfully completed!')
+    $('.signup-form').css({
+        'display': 'none'
+    });
+})
